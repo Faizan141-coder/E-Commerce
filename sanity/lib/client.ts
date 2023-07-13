0d3981f0293c677d8878c5f54130d84fd7fa129f
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-07-13 12:10:53 +0500

    commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from "next-sanity"
-
 import { apiVersion, dataset, projectId, useCdn } from "../env"
 
 export const client = createClient({
-  apiVersion,
-  dataset,
   projectId,
+  dataset,
+  apiVersion,
   useCdn,
+  token: "skozx10kz9tMi0LGmvwkjx86p3A54Hvc4aSGHsVHb46QZncPTaTk4XQlWohQyPUhckVCjuIDbIT4JaoBxomG7e9fc39BQT1NXXwIN2mvIcpmpVfdf4hiy0lCeKLjwLbAT5ikVv0kQCOlqZoYRpzu1BGfRMOIb7i5NGTAsSFwbg4CUP5DcmDQ",
 })
