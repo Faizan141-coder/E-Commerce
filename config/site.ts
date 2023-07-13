c1ef253b29977bc4e72723716e27d3f8268045ad
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-07-13 10:49:49 +0500

    set up sanity

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "EasyBuy",
+  description: "Discover an effortless shopping experience at EasyBuy, your go-to destination for convenient online purchases.",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -1,1 +1,3 @@
-export const product = {}
+export const product = {
+    
+}
