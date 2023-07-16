b7ca67b6edd1deb82cc83a271251ad3e4e96a91c
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-07-16 18:56:56 +0500

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,4 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn,
-  token: "skozx10kz9tMi0LGmvwkjx86p3A54Hvc4aSGHsVHb46QZncPTaTk4XQlWohQyPUhckVCjuIDbIT4JaoBxomG7e9fc39BQT1NXXwIN2mvIcpmpVfdf4hiy0lCeKLjwLbAT5ikVv0kQCOlqZoYRpzu1BGfRMOIb7i5NGTAsSFwbg4CUP5DcmDQ",
 })
